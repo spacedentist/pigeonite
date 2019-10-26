85cda0231a7294d7d6184b2cf849b41e7cd74710
Sven Over <sven@svenover.de>
2019-10-26 13:38:14 +0100

    StateLoggerPlugin: add `pretty` option, for pretty printing

--- pykzee/StateLoggerPlugin.py
+++ pykzee/StateLoggerPlugin.py
@@ -1,11 +1,59 @@
 import logging
 
+from pyimmutable import ImmutableDict, ImmutableList
 from pykzee.Plugin import Plugin
 
 
 class StateLoggerPlugin(Plugin):
-    def init(self, path=()):
+    def init(self, path=(), *, pretty=False):
+        self.__pretty = pretty
         self.unsubscribe = self.subscribe(self.stateUpdate, path)
 
     def stateUpdate(self, state):
-        logging.debug(repr(state))
+        if not self.__pretty:
+            logging.debug(repr(state))
+            return
+
+        logging.debug("StateLoggerPlugin: new state:")
+        pretty_print(state, OutputLines(logging.debug))
+
+
+class OutputLines:
+    def __init__(self, write):
+        self.__write = write
+        self.__data = ""
+
+    def __call__(self, x):
+        self.__data += x
+        pos = self.__data.rfind("\n")
+        if pos < 0:
+            return
+        out = self.__data[0:pos]
+        self.__data = self.__data[pos + 1 :]
+        for line in out.split("\n"):
+            self.__write(line)
+
+    def __del__(self):
+        if self.__data:
+            self.__write(self.__data)
+
+
+def pretty_print(data, write, indent=""):
+    if type(data) is ImmutableDict:
+        more_indent = indent + "  "
+        write("{\n")
+        for key, value in data.items():
+            write(f"{ indent }  { key !r}: ")
+            pretty_print(value, write, more_indent)
+            write(",\n")
+        write(f"{ indent }}}")
+    elif type(data) is ImmutableList:
+        more_indent = indent + "  "
+        write("[\n")
+        for value in data:
+            write(more_indent)
+            pretty_print(value, write, more_indent)
+            write(",\n")
+        write(f"{ indent }]")
+    else:
+        write(repr(data))
