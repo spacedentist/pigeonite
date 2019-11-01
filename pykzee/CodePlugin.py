06acb1a27755db4dfa50ffa074aacd3d000a0f05
Sven Over <sven@svenover.de>
2019-11-01 15:28:46 +0000

    Add pykzee.CodePlugin
    This is a plugin that allows to easily implement logic for home automation
    inside the configuration tree. A CodePlugin instance expects a
    configuration key `code.py` to contain Python code. The code is executed
    and can e.g. subscribe to state changes (via the `subscribe` global
    function).

---
+++ pykzee/CodePlugin.py
@@ -0,0 +1,70 @@
+import builtins
+import functools
+import inspect
+import traceback
+
+from pykzee.common import call_soon, Undefined
+from pykzee.Plugin import Plugin
+
+
+environment = {
+    key: value
+    for key, value in builtins.__dict__.items()
+    if key
+    not in (
+        "__dict__",
+        "__import__",
+        "__loader__",
+        "__name__",
+        "__package__",
+        "__spec__",
+        "eval",
+        "exec",
+        "exit",
+        "open",
+    )
+}
+environment.update(Undefined=Undefined, call_soon=call_soon)
+
+
+class CodePlugin(Plugin):
+    def init(self, config):
+        try:
+            code = compile(config["code.py"], "code.py", "exec")
+            globals = {
+                "__builtins__": dict(
+                    environment,
+                    path=self.path,
+                    get=self.get,
+                    subscribe=self.subscribe,
+                    command=self.command,
+                    set_state=self.set,
+                    register_command=self.registerCommand,
+                    state_from_subscription=self.stateFromSubscription,
+                )
+            }
+            exec(code, globals)
+        except Exception as ex:
+            self.set(
+                (), {"exception": str(ex), "traceback": traceback.format_exc()}
+            )
+
+    def stateFromSubscription(self, handler, *paths):
+        handler = functools.partial(self.subscriptionCallback, handler)
+        if paths:
+            self.set((), "Waiting for subscription...")
+            return self.subscribe(handler, *paths)
+        else:
+            call_soon(handler)
+            return lambda: None
+
+    async def subscriptionCallback(self, handler, *state):
+        try:
+            ret = handler(*state)
+            if inspect.isawaitable(ret):
+                ret = await ret
+            self.set((), ret)
+        except Exception as ex:
+            self.set(
+                (), {"exception": str(ex), "traceback": traceback.format_exc()}
+            )
